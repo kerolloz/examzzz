7accaf09e37c2461379cd09c3eaed166b5df2515
Kerollos Magdy <kerolloz@yahoo.com>
2023-07-21 12:26:27 +0300

    add admin table

---
+++ src/database/prisma/migrations/20230721092617_add_admin_table/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Admin" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "Admin_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Admin_username_key" ON "Admin"("username");
