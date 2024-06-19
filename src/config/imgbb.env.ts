c71bfd2bc19b5dce2b53bbea7ca230492b19e26a
Kerollos Magdy <kerolloz@yahoo.com>
2024-06-19 15:07:02 +0300

    chore: Remove unused IMGBB_KEY from .env.example and related code

--- src/config/imgbb.env.ts
+++
@@ -1,3 +0,0 @@
-import env from './env';
-
-export const imgbbKeyEnvVar = env('IMGBB_KEY');
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,2 @@
 export { default as env } from './env';
-export * from './imgbb.env';
 export * from './jwt.env';
